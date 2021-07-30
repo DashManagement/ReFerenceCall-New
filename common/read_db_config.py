723f040685f3d35d0be9df63b1f9435b90815b52
樊少强 <shaoqiang.fan@gmail.com>
2021-07-30 10:45:26 +0800

    调整数据库的配制，添加一个debug的选项

--- common/read_db_config.py
+++ common/read_db_config.py
@@ -2,8 +2,8 @@
 @Description:
 @Author: michael
 @Date: 2021-07-08 10:16:20
-LastEditTime: 2021-07-08 20:00:00
-LastEditors: michael
+LastEditTime: 2021-07-30 10:30:38
+LastEditors: fanshaoqiang
 '''
 # coding=utf-8
 
@@ -13,47 +13,35 @@ import motor.motor_asyncio
 import pymongo
 
 readConfig = ConfigParser()
+debug = 0
+if debug == 0:
+    # 加载远程数据库配置变量
+    readConfig.read('./config/database.ini')
+    host = readConfig.get('remote_config', 'host')
+    port = readConfig.getint('remote_config', 'port')
+    user = readConfig.get('remote_config', 'user')
+    password = readConfig.get('remote_config', 'password')
+    db = readConfig.get('remote_config', 'db')
+    prefix = readConfig.get('remote_config', 'prefix')
+    collection_name = readConfig.get('remote_config', 'collection')
 
+    # 组合远程数据库地址
+    mongo_url = f"mongodb://{user}:{password}@{host}:{port}/{prefix}{db}"
+else:
+    # 加载 本地 localhost 数据库配置变量
+    readConfig.read('./config/database.ini')
+    host = readConfig.get('localhost_config', 'host')
+    port = readConfig.getint('localhost_config', 'port')
+    user = readConfig.get('localhost_config', 'user')
+    password = readConfig.get('localhost_config', 'password')
+    db = readConfig.get('localhost_config', 'db')
+    prefix = readConfig.get('localhost_config', 'prefix')
+    collection_name = readConfig.get('localhost_config', 'collection')
 
-# 加载远程数据库配置变量
-# readConfig.read('./config/database.ini')
-# host = readConfig.get('remote_config', 'host')
-# port = readConfig.getint('remote_config', 'port')
-# user = readConfig.get('remote_config', 'user')
-# password = readConfig.get('remote_config', 'password')
-# db = readConfig.get('remote_config', 'db')
-# prefix = readConfig.get('remote_config', 'prefix')
-# collection_name = readConfig.get('remote_config', 'collection')
+    # 组合本地数据库地址
+    mongo_url = f"mongodb://{host}:{port}"
 
-# 组合远程数据库地址
-# mongo_url = f"mongodb://{user}:{password}@{host}:{port}/{db}"
-
-# 加载 本地 localhost 数据库配置变量
-readConfig.read('./config/database.ini')
-host = readConfig.get('localhost_config', 'host')
-port = readConfig.getint('localhost_config', 'port')
-user = readConfig.get('localhost_config', 'user')
-password = readConfig.get('localhost_config', 'password')
-db = readConfig.get('localhost_config', 'db')
-prefix = readConfig.get('localhost_config', 'prefix')
-collection_name = readConfig.get('localhost_config', 'collection')
-
-# 加载 服务器 server 数据库配置变量
-# readConfig.read('./config/database.ini')
-# host = readConfig.get('remote_config', 'host')
-# port = readConfig.getint('remote_config', 'port')
-# user = readConfig.get('remote_config', 'user')
-# password = readConfig.get('remote_config', 'password')
-# db = readConfig.get('remote_config', 'db')
-# prefix = readConfig.get('remote_config', 'prefix')
-# collection_name = readConfig.get('remote_config', 'collection')
-
-# 组合本地数据库地址
-mongo_url = f"mongodb://{host}:{port}"
-
-# 组合远程数据库地址
-# mongo_url = f"mongodb://{user}:{password}@{host}:{port}"
-
+print(mongo_url)
 # 远程数据库地址 mongoClient 示例
 # emote_client = MongoClient('mongodb://dash:dashmima!@118.193.47.247:8088/dash')
 
