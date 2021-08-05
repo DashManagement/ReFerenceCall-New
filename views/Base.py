e05146b89de88b995c3f815d96e6955e59cb509d
樊少强 <shaoqiang.fan@gmail.com>
2021-08-05 11:42:35 +0800

    添加一个根据UID获取用户Push用数据的函数

--- views/Base.py
+++ views/Base.py
@@ -2,8 +2,8 @@
 @Description:
 @Author: michael
 @Date: 2021-07-08 10:16:20
-LastEditTime: 2021-07-08 20:00:00
-LastEditors: michael
+LastEditTime: 2021-08-05 11:22:21
+LastEditors: fanshaoqiang
 '''
 # coding=utf-8
 
@@ -14,8 +14,8 @@ LastEditors: michael
 from common.register_class import *
 from config.log_config import logger
 
-class Base:
 
+class Base:
 
     # 根据 id 验证是否有此用户
     async def verifyUser(self, uid=''):
@@ -25,16 +25,16 @@ class Base:
 
         # 条件 - 用户名 - 返回字段 全部
         condition = {'id': uid}
-        field = {'id':1, '_id': 0}
+        field = {'id': 1, '_id': 0}
         user_info = await dbo.findOne(condition, field)
-        
+
         if user_info is None:
             return False
 
         return True
 
-
     # 根据 id 验证是否有此用户 - 并返回用户信息
+
     async def verifyUserReturnInfo(self, uid=''):
 
         # 连接数据库
@@ -44,14 +44,14 @@ class Base:
         condition = {'id': uid}
         field = {'_id': 0}
         user_info = await dbo.findOne(condition, field)
-        
+
         if user_info is None:
             return False
 
         return user_info
 
-
     # 根据 id 验证是否有此公司 - 并返回公司信息
+
     async def verifyFundCompany(self, company_id=''):
 
         # 连接数据库
@@ -59,8 +59,10 @@ class Base:
 
         # 条件 - 用户名 - 返回字段 全部
         # condition = {"$where": "this.id == this.company_id", "describe":"0", "company_id":company_id}
-        condition = {"id":company_id, "company_id":company_id, "describe":"0"}
-        field = {'id':1, 'company_id':1, 'fund_name':1, 'company_icon':1, 'base_info':1, 'company_info':1, '_id':0}
+        condition = {"id": company_id,
+                     "company_id": company_id, "describe": "0"}
+        field = {'id': 1, 'company_id': 1, 'fund_name': 1,
+                 'company_icon': 1, 'base_info': 1, 'company_info': 1, '_id': 0}
         company_info = await dbo.findOne(condition, field)
         print(company_info)
         print(123123)
@@ -70,10 +72,21 @@ class Base:
 
         return company_info
 
+    # 返回用户Push相关的参数
 
+    async def getUserPushInfo(self, uid):
+        # 连接数据库
+        dbo.resetInitConfig('test', 'users')
 
+        # 条件 - 用户名 - 返回字段 全部
+        condition = {'id': uid}
+        field = {'_id': 0}
+        user_info = await dbo.findOne(condition, field)
 
+        if user_info is None:
+            return None
+
+        return {"userToken": user_info.get("userToken"), "localTimeZone": user_info.get("localTimeZone"), "platForm": user_info.get("platForm")}
 
 
 base = Base()
-
