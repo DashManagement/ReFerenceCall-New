c8f880ada1c6eab0ed84b1e434ac322c21ddee9a
michael <26152462@qq.com>
2021-08-10 18:07:07 +0800

    临时提交，请勿拉取本次提交

--- views/reference_call_api/CompanyCurd.py
+++ views/reference_call_api/CompanyCurd.py
@@ -152,7 +152,7 @@ class CompanyCurd:
     # 按公司查看 reference_call 的志愿者列表
 
     async def companyVolunteersList(self, id, company_id):
-        return await timeOperation.timeList()
+
         data = {
             'company_info': {},
             'volunteers_list': []
@@ -169,17 +169,20 @@ class CompanyCurd:
         else:
             data['company_info'] = company_info
 
-        # 查询志愿者列表
+        # 查询志愿者 id 列表
         dbo.resetInitConfig('test', 'reference_call_company')
         condition = {'rc_company_id': company_id}
         field = {'uid': 1, '_id': 0}
 
-        company_volunteers_list = await dbo.getData(condition, field)
-
+        company_volunteers_id_list = await dbo.getData(condition, field)
+        
+        # 查看志愿者是否有多余的时间来处理预约会议
+        check_volunteers_time = await self.checkVolunteersTime(company_volunteers_id_list)
+        return check_volunteers_time
         # 查询志愿者详细信息
         data['volunteers_list'] = []
         dbo.resetInitConfig('test', 'users')
-        for value in company_volunteers_list:
+        for value in company_volunteers_id_list:
             '''根据 uid 查询公司匹配的志愿者信息（不包括当前用户id 的志愿者，也就是排除志愿者自己）'''
             condition = {'$and': [
                 {'id': int(value['uid'])},
@@ -198,6 +201,88 @@ class CompanyCurd:
         logger.info(f"data is {data}")
         return {'code': 200, 'data': data}
 
+
+    # 查看志愿者是否有多余的时间来处理预约会议
+    # async def checkVolunteersTime(self, id, company_volunteers_id_list):
+    #     print(company_volunteers_id_list)
+
+
+    # 查看志愿者是否有多余的时间来处理预约会议
+    async def checkVolunteersTime(self, company_volunteers_id_list):
+        # print(type(id))
+        # return id
+        # 获取第一次发起请求的所有数据
+        # condition = {self.query_field: self.id, 'request_num': 1}
+        # field = {'session_id': 1, '_id': 0}
+        # result = await dbo.getData(condition, field)
+
+        # 获取第一次发起请求的所有数据
+        dbo.resetInitConfig('test', 'reservation_meeting')
+        condition = {'request_num': 1}
+        field = {'session_id': 1, '_id': 0}
+        result = await dbo.getData(condition, field)
+        print(result)
+
+         # 如果没有记录则直接返回空的 list 列表
+        if len(result) == 0:
+            return []
+
+        # 获取单个 session_id 的所有预约记录的最后一条
+        session_id_record = []
+        for value in result:
+            condition = {'session_id': value['session_id']}
+            field = {
+                "id": 1,
+                "reservation_company_id": 1,
+                "reservation_company_name": 1,
+                "session_id": 1,
+                "start_id": 1,
+                "start_user_name": 1,
+                "start_head_portrait": 1,
+                # "start_working_fixed_year": 1,
+                "start_company_name": 1,
+                "start_company_icon": 1,
+                "end_id": 1,
+                "end_user_name": 1,
+                "end_head_portrait": 1,
+                # "end_working_fixed_year": 1,
+                "end_company_name": 1,
+                "end_company_icon": 1,
+                # "meeting_pass": 1,
+                # "national_area_code": 1,
+                # "national_area_name": 1,
+                # "meeting_time": 1,
+                # "meeting_status": 1,
+                "volunteer_reply_time": 1,
+                "volunteer_reply_time": 1,
+                "requester_agree_time": 1,
+                "request_num": 1,
+                "current_id": 1,
+                "is_create_meeting": 1,
+                "status": 1,
+                "create_time": 1,
+                "_id": 0
+            }
+            sort = [('id', -1)]
+            num = 0
+            session_id_record.append(await dbo.findSort(condition, field, sort, num))
+
+        # return session_id_record
+
+        meeting_list = []
+        booking_list = []
+        for value_two in session_id_record:
+            value_two = value_two[0]
+
+            '''添加已经预约的议列表'''
+            if value_two['is_create_meeting'] == 1:
+                meeting_list.append(value_two)
+
+            '''添加正在进行中的预约，并且状态为有效，并且者愿者已经回复了时间可用时间'''
+            if value_two['is_create_meeting'] == 0 and value_two['status'] == 1 and value_two['request_num'] == 2:
+                booking_list.append(value_two)
+
+        return {'meeting_list':meeting_list, 'booking_list':booking_list}
 
 
 
