9379875a7bb3d2b49a30a8ce90bf95d7724c1692
樊少强 <shaoqiang.fan@gmail.com>
2021-08-11 12:04:28 +0800

    修改返回值里面code写错成coce的问题

--- views/Meeting.py
+++ views/Meeting.py
@@ -2,8 +2,8 @@
 @Description:
 @Author: michael
 @Date: 2021-08-02 10:16:20
-LastEditTime: 2021-08-02 20:00:00
-LastEditors: michael
+LastEditTime: 2021-08-11 11:59:35
+LastEditors: fanshaoqiang
 '''
 # coding=utf-8
 
@@ -18,57 +18,57 @@ from views.meeting_api.LastCall import lastCall
 from views.meeting_api.HistoryMeetingSchedule import historyMeetingSchedule
 
 # Meeting 会议接口类
-class Meeting:
 
+
+class Meeting:
 
     # 第一次预约会议
     async def sendRequest(self, id, volunteers_id, request_type, reservation_company_id, reservation_company_name):
 
         if len(volunteers_id) < 1:
-            return {'code':207, 'message':'预约会议列表不合法'}
+            return {'code': 207, 'message': '预约会议列表不合法'}
 
         data = []
         for value in volunteers_id:
             result = await meetingFirstMeetingRequest.construct(id, value, request_type, reservation_company_id, reservation_company_name)
-            result['send_request_info'] = {'id':id, 'volunteers_id':value}
+            result['send_request_info'] = {'id': id, 'volunteers_id': value}
             data.append(result)
 
-        return {'coce':200, 'data':data}
-
+        return {'code': 200, 'data': data}
 
     # 志愿者回复预约时间或者拒绝
+
     async def volunteerReplyRequest(self, id, session_id, request_type, volunteer_reply_time):
         return await meetingVolunteerReplyRequest.construct(id, session_id, request_type, volunteer_reply_time)
 
-
     # 请求者同意预约时间或者拒绝
+
     async def requesterRequest(self, id, session_id, request_type, requester_agree_time):
         return await meetingRequesterRequest.construct(id, session_id, request_type, requester_agree_time)
 
-
     # 查看预约会议请求
+
     async def checkRequest(self, id, request_type, data_num):
         return await bookingMeetingList.construct(id, request_type, data_num)
 
-
     # 获取会议列表相关操作
+
     async def getMeetingList(self, id, request_type, check_type, data_num):
         return await meetingList.construct(id, request_type, check_type, data_num)
 
-
     # 已经约定的会议日程
+
     async def meetingSchedule(self, id):
         return await meetingSchedule.construct(id)
 
-
     # 最后一个已完成的会议信息接口
+
     async def lastCall(self, id):
         return await lastCall.construct(id)
 
     # 最后一个已完成的会议信息接口
     async def historyMeetingSchedule(self, id):
         return await historyMeetingSchedule.construct(id)
-
 
 
 meeting = Meeting()
