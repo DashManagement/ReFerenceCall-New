f8f21a8e1e4f71e10a020ecdc274ce0f6da57f98
michael <26152462@qq.com>
2021-08-12 10:20:39 +0800

    更改配置文件

--- common/read_db_config.py
+++ common/read_db_config.py
@@ -13,7 +13,7 @@ import motor.motor_asyncio
 import pymongo
 
 readConfig = ConfigParser()
-distribution = 0
+distribution = 1
 # 0 代表 读
 if distribution:
     readConfig.read('./config/database.ini')
@@ -27,7 +27,7 @@ if distribution:
     # 组合远程数据库地址
     mongo_url = f"mongodb://{user}:{password}@{host}:{port}/{prefix}{db}"
 else:
-    useLocalMongo = 1
+    useLocalMongo = 0
     if useLocalMongo == 0:
         # 加载远程数据库配置变量
         readConfig.read('./config/database.ini')
