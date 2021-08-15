3a11254696865fede5c87150cea2e7f160163537
michael <26152462@qq.com>
2021-08-15 17:21:57 +0800

    更改 history_meeting_schedule 接口 的返回对应会议人员返回名称

--- views/meeting_api/HistoryMeetingSchedule.py
+++ views/meeting_api/HistoryMeetingSchedule.py
@@ -36,6 +36,10 @@ class HistoryMeetingSchedule:
     # 查询已经约定的会议日程接口
     async def getMeetingScheduleList(self, user_info):
 
+        # 今日零点时间戳
+        today_zero_stamp = common.getTimeStamp()
+        '''后面会用今日零点的时间戳代替当前时间 common.getTime()，暂时只是测试用的'''
+
         dbo.resetInitConfig('test', 'meeting_list')
         condition = {'create_time':{'$lt' : common.getTime()}, '$or':[
             {'start_id':self.id},
@@ -45,11 +49,22 @@ class HistoryMeetingSchedule:
             "id": 1,
             "reservation_company_id": 1,
             "reservation_company_name": 1,
+            "start_id": 1,
+            "start_user_name": 1,
+            "start_head_portrait": 1,
+            "start_company_name": 1,
+            "start_company_icon": 1,
+            "end_id": 1,
+            "end_user_name": 1,
+            "end_head_portrait": 1,
+            "end_company_name": 1,
+            "end_company_icon": 1,
             "session_id": 1,
             "start_time": 1,
             "meeting_id": 1,
             "meeting_pass": 1,
             "meeting_address": 1,
+            "meeting_time": 1,
             'create_time':1,
             '_id':0
         }
@@ -60,7 +75,32 @@ class HistoryMeetingSchedule:
             value['name'] = user_info['name']
             value['company_name'] = user_info['company_name']
 
-        return result
+        # 整理用户信息返回的字段
+        return_list = await self.finishingReturnUserInfoField(result)
+
+        return return_list
+
+
+     # 整理用户信息字段
+    async def finishingReturnUserInfoField(self, schedule_list):
+
+        for value in schedule_list:
+            # for meeting in value['meeting']['list']:
+            if self.id == value['end_id']:
+                value['name'] = value['start_user_name']
+                value['head_portrait'] = value['start_head_portrait']
+                value['company_name'] = value['start_company_name']
+                value['company_icon'] = value['start_company_icon']
+            if self.id == value['start_id']:
+                value['name'] = value['end_user_name']
+                value['head_portrait'] = value['end_head_portrait']
+                value['company_name'] = value['end_company_name']
+                value['company_icon'] = value['end_company_icon']
+
+            del value['start_user_name'], value['start_head_portrait'], value['start_company_name'], value['start_company_icon']
+            del value['end_user_name'], value['end_head_portrait'], value['end_company_name'], value['end_company_icon']
+
+        return schedule_list
 
 
     # 获取用户信息
